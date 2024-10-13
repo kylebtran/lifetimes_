af331905281e84d2d6ad989ff5e742d0b6b5434d
Zachary Bi <BiZac23@uw.edu>
2024-10-12 22:25:51 -0700

    Added 02 for bento

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,31 +1,37 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "#1A1717",
-        middleground: "#282323",
-        foreground: "var(--foreground)",
-        muted: "#8B8484",
-        accent: "#FFC926",
-        panels: "#2D2B2B",
-        confirm: "#31AE57",
-      },
-      fontFamily: {
-        sans: ["var(--font-dm-sans)", "sans-serif"],
-        serif: ["var(--font-zilla-slab)", "serif"],
-      },
-      gridTemplateColumns: {
-        "auto-51": "repeat(51, auto)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: '#1A1717',
+  			middleground: '#282323',
+  			foreground: 'var(--foreground)',
+  			muted: '#8B8484',
+  			accent: '#FFC926',
+  			panels: '#2D2B2B',
+  			confirm: '#31AE57'
+  		},
+  		fontFamily: {
+  			sans: ["var(--font-dm-sans)", "sans-serif"],
+  			serif: ["var(--font-zilla-slab)", "serif"]
+  		},
+  		gridTemplateColumns: {
+  			'auto-51': 'repeat(51, auto)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
