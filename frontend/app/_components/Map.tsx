4e2f20ffc0487371960c33947011669c4772e44c
kylebtran <ucitrankyl@gmail.com>
2024-10-12 20:06:04 -0700

    Appended map to square.

--- frontend/app/_components/Map.tsx
+++ frontend/app/_components/Map.tsx
@@ -22,100 +22,101 @@ function Map({ coords }: { coords: number[][] }) {
       const trace1 = {
         x,
         y,
-        mode: 'markers', // Include markers for points
-        name: 'points',
+        mode: "markers", // Include markers for points
+        name: "points",
         marker: {
-          color: 'rgb(0, 0, 0)',
+          color: "rgb(0, 0, 0)",
           size: 20,
           opacity: 0.4,
         },
-        type: 'scatter',
-        hoverinfo: 'x+y', // Show text hover info
+        type: "scatter",
+        hoverinfo: "x+y", // Show text hover info
       };
 
       const trace3 = {
         x,
         y,
-        mode: 'lines', // Include markers for points
-        name: 'points',
+        mode: "lines", // Include markers for points
+        name: "points",
         line: {
-            color: 'rgb(255, 255, 255)',
-            width: 1,
+          color: "rgb(255, 255, 255)",
+          width: 1,
         },
         opacity: 0.3,
-        type: 'scatter',
+        type: "scatter",
       };
 
       const trace2 = {
         x,
         y,
-        name: 'density',
+        name: "density",
         ncontours: 8,
         colorscale: [
-
-          [0, 'rgba(26, 23, 23, 0.3)'],
-          [0.9, 'rgba(26, 23, 23, 0.1)'],
-          [1, 'rgba(0, 0, 0, 0)'],
+          [0, "rgba(26, 23, 23, 0.3)"],
+          [0.9, "rgba(26, 23, 23, 0.1)"],
+          [1, "rgba(0, 0, 0, 0)"],
         ],
         reversescale: true,
         showscale: false,
-        type: 'histogram2dcontour',
-        hoverinfo: 'none', // Disable hover info for density trace
+        type: "histogram2dcontour",
+        hoverinfo: "none", // Disable hover info for density trace
         contours: {
           showlines: false,
         },
         marker: {
-          color: 'rgb(102,0,0)',
+          color: "rgb(102,0,0)",
           size: 5,
           opacity: 0.4,
         },
       };
 
-      const config = { displayModeBar: false };
+      const config = { displayModeBar: false, responsive: true };
       const data = [trace2, trace3, trace1];
 
       const layout = {
         showlegend: false,
         autosize: false,
-        width: 1500,
-        height: 1000,
-        hovermode: 'closest',
+        width: 984,
+        height: 875,
+        hovermode: "closest",
         bargap: 0,
-        paper_bgcolor: 'rgba(0,0,0,0)',
-        plot_bgcolor: 'rgba(0,0,0,0)',
+        paper_bgcolor: "rgba(0,0,0,0)",
+        plot_bgcolor: "rgba(0,0,0,1)",
         xaxis: {
-            visible: false,
-            domain: [0, 0.85],
-            showgrid: false,	
-            zeroline: false
-          },
-          yaxis: {
-            visible: false,
-            domain: [0, 0.85],
-            showgrid: false,
-            zeroline: false
-          },
-          xaxis2: {
-            visible: false,
-            domain: [0.85, 1],
-            showgrid: false,
-            zeroline: false
-          },
-          yaxis2: {
-            visible: false,
-            domain: [0.85, 1],
-            showgrid: false,
-            zeroline: false
-          },
-          dragmode: false,
+          visible: true,
+          domain: [0, 0.85],
+          showgrid: false,
+          zeroline: false,
+          range: [-1, 1],
+        },
+        yaxis: {
+          visible: true,
+          domain: [0, 0.85],
+          showgrid: false,
+          zeroline: false,
+          range: [-1, 1],
+        },
+        xaxis2: {
+          visible: false,
+          domain: [0.85, 1],
+          showgrid: false,
+          zeroline: false,
+        },
+        yaxis2: {
+          visible: false,
+          domain: [0.85, 1],
+          showgrid: false,
+          zeroline: false,
+        },
+        dragmode: false,
       };
 
-      window.Plotly.newPlot('myDiv2', data, layout, config);
+      window.Plotly.newPlot("myDiv2", data, layout, config);
     }
   }, [plotlyLoaded, coords]);
 
   return (
-    <div id='myDiv2'>
+    <div id="myDiv2" style={{ width: "100%", height: "100%" }}>
       {/* <div>X coordinates: {coords.map(a => a[0]).join(', ')}</div>
       <div>Y coordinates: {coords.map(a => a[1]).join(', ')}</div> */}
     </div>
